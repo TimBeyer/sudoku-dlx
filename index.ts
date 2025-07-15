698ae0dfc0576686c2214ce1207319f916ff63a5
Tim Beyer <tim.beyer@gmail.com>
2025-07-15 17:34:56 +0200

    feat: modernize and ESM

--- index.ts
+++ index.ts
@@ -1,35 +1,44 @@
-import { SudokuCell, generateConstraints, parseStringFormat } from './lib'
-import { findRaw } from 'dancing-links'
+import {
+  SudokuCell,
+  generateConstraints,
+  parseStringFormat,
+} from "./lib/index.js";
+import { findRaw } from "dancing-links";
 
-const FIELD_SIZE = 9
+const FIELD_SIZE = 9;
 
-export function solveString (sudoku: string, all = false): SudokuCell[][] {
-  const totalConstraints = (FIELD_SIZE * FIELD_SIZE) * 4
-  const cells = parseStringFormat(sudoku, FIELD_SIZE)
-  const constraints = generateConstraints(cells, FIELD_SIZE)
-
-  const result = findRaw({
-    numPrimary: totalConstraints,
-    numSecondary: 0,
-    numSolutions: all ? Infinity : 1,
-    rows: constraints
-  })
-
-  return result.map((r) => r.map((s) => s.data))
-}
-
-export function solveCells (sudoku: SudokuCell[], all = false): SudokuCell[][] {
-  const totalConstraints = (FIELD_SIZE * FIELD_SIZE) * 4
-  const constraints = generateConstraints(sudoku, FIELD_SIZE)
+export function solveString(sudoku: string, all = false): SudokuCell[][] {
+  const totalConstraints = FIELD_SIZE * FIELD_SIZE * 4;
+  const cells = parseStringFormat(sudoku, FIELD_SIZE);
+  const constraints = generateConstraints(cells, FIELD_SIZE);
 
   const result = findRaw({
     numPrimary: totalConstraints,
     numSecondary: 0,
     numSolutions: all ? Infinity : 1,
-    rows: constraints
-  })
+    rows: constraints,
+  });
 
-  return result.map((r) => r.map((s) => s.data))
+  return result.map((r) => r.map((s) => s.data));
 }
 
-export { SudokuCell, parseStringFormat, generateConstraints, printBoard } from './lib/index'
+export function solveCells(sudoku: SudokuCell[], all = false): SudokuCell[][] {
+  const totalConstraints = FIELD_SIZE * FIELD_SIZE * 4;
+  const constraints = generateConstraints(sudoku, FIELD_SIZE);
+
+  const result = findRaw({
+    numPrimary: totalConstraints,
+    numSecondary: 0,
+    numSolutions: all ? Infinity : 1,
+    rows: constraints,
+  });
+
+  return result.map((r) => r.map((s) => s.data));
+}
+
+export {
+  SudokuCell,
+  parseStringFormat,
+  generateConstraints,
+  printBoard,
+} from "./lib/index.js";
